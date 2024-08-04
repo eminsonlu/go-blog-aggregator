11b8fbd350466b98ad5d03b05dfa3ec9f0173c1f
Emin Sonlu <eminsnl2@gmail.com>
2024-08-04 15:33:00 +0300

    Add serve mux and respond function, Add healthz api

--- main.go
+++ main.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	port := os.Getenv("PORT")
+
+	serveMux := http.NewServeMux()
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: serveMux,
+	}
+
+	serveMux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World"))
+	})
+	serveMux.HandleFunc("/v1/healthz", handlerHealthz)
+	serveMux.HandleFunc("/v1/err", handlerError)
+
+	log.Println("Server running on port", port)
+
+	err = server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
